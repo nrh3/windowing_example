37ce864033d68657c36ddb0ed23a4219a8243c6b
Nat Howard <nrh@pupworks.com>
2018-03-06 15:56:47 -0500

    startup

---
+++ randomttimestep.sql
@@ -0,0 +1,33 @@
+-- Illustration of use of window functions arising from Real Life by Nat Howard for Stephen Frost  3/6/18.
+
+drop table if exists incoming_pings;
+
+create table incoming_pings (
+	when_pung	timestamptz -- not primary key because it could be non-unique in the "real" case!
+);
+
+
+-- Fill the incoming_pings table with timestamps in  generate a list of ascending time stamps
+-- Adapted from https://gist.github.com/palpha/8333162 - thanks Palpha!
+with recursive t (ts) as (
+  with base as (
+    with settings as (
+      select
+        now() as max
+        , array[now() - '1 year'::interval, now() - '390 days'::interval] as start_range
+        , array['0 seconds'::interval , '2 hours'::interval] as step_range
+    )
+    select
+      (random() * (start_range[2] - start_range[1]) + start_range[1]) as start
+      , step_range
+      , max
+    from settings
+  )
+  select start from base
+  union all
+  select ts + (random() * (step_range[2] - step_range[1]) + step_range[1])
+  from t cross join base
+  where ts < max
+)
+select ts into incoming_pings
+from t;
