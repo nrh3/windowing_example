8b95bd393b74d07f34acb58583561c402ada8b8f
Nat Howard <nrh@pupworks.com>
2018-03-06 16:58:17 -0500

    Seemingly working.

--- 1_build_ping_table.sql
+++ 1_build_ping_table.sql
@@ -1,6 +1,6 @@
--- Illustration of use of window functions arising from Real Life by Nat Howard for Stephen Frost  3/6/18.
+-- Illustration of use of postgresql window functions arising from Real Life by Nat Howard 
 
--- for reproducible results -- we set the random seed
+-- for reproducible results -- we set the random seed, and instead of "now()" we'll use '2018-03-06 21:54:12.857133+00'::timestamptz
 
 set seed to 0.5;
 
@@ -18,8 +18,8 @@ with recursive t (ts) as (
   with base as (
     with settings as (
       select
-        now() as max
-        , array[now() - '1 year'::interval, now() - '390 days'::interval] as start_range
+        '2018-03-06 21:54:12.857133+00'::timestamptz  as max
+        , array['2018-03-06 21:54:12.857133+00'::timestamptz - '1 year'::interval, '2018-03-06 21:54:12.857133+00'::timestamptz - '390 days'::interval] as start_range
         , array['0 seconds'::interval , '2 hours'::interval] as step_range
     )
     select
@@ -41,13 +41,13 @@ from t;
 -- You might want to keep this part secret from the audience
 -- Now the ping table is loaded, simulate a bunch of data failures by a couple of random things
 -- For example, just six months ago, we had a two-day outage:
-delete from incoming_pings where when_pung between now() - '6 months 2 days'::interval and now() - '6 months'::interval;
+delete from incoming_pings where when_pung between '2018-03-06 21:54:12.857133+00'::timestamptz - '6 months 2 days'::interval and '2018-03-06 21:54:12.857133+00'::timestamptz - '6 months'::interval;
 
 -- And then, because of a peculiar habit of a lab technician we hired a month ago (he prefers to use all the internet bandwidth  between 
 -- 8pm and midnight on thursdays) we are also experiencing outages then.
 
--- select * from incoming_pings where extract(dow from when_pung) = 4 and extract(hour from when_pung) is between 20 and 23 and when_pung > now() - '1 month'::interval ;
+-- select * from incoming_pings where extract(dow from when_pung) = 4 and extract(hour from when_pung) is between 20 and 23 and when_pung > '2018-03-06 21:54:12.857133+00'::timestamptz - '1 month'::interval ;
 
-delete   from incoming_pings where extract(dow from when_pung) = 4 and extract(hour from when_pung)  between 20 and 23 and when_pung > now() - '1 month'::interval ;
+delete   from incoming_pings where extract(dow from when_pung) = 4 and extract(hour from when_pung)  between 20 and 23 and when_pung > '2018-03-06 21:54:12.857133+00'::timestamptz - '1 month'::interval ;
 
 
